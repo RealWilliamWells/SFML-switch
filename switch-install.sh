61c634bb35a925386ecf4a2c001a02b8773f7f0e
Alexander Winter <a.w1nter@hotmail.com>
2022-04-01 19:39:45 -0400

    Install script

---
+++ switch-install.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+
+set -eu
+cmake -B build-switch -S . -DCMAKE_TOOLCHAIN_FILE=../cmake/toolchains/DevkitA64Libnx.cmake
+cd build-switch/
+make
+cd ..
+
+sudo cp build/lib/* $DEVKITPRO/portlibs/switch/lib
+sudo cp -r include/SFML/ $DEVKITPRO/portlibs/switch/include/
